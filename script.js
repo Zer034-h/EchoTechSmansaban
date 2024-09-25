89632fe9cccbc30a867927b80878eed838aab641
Zer034-h <zero.ltr34@gmail.com>
2024-09-25 13:41:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ const firebaseConfig = {
   messagingSenderId: "534573831564",
   appId: "1:534578381564:web:6ed12537db0c135bf84d79"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // Reference to the Firebase Realtime Database
 const database = firebase.database();
 
-// Function to update data on the webpage
+// Fungsi untuk memperbarui data di halaman
 function updateSensorData(snapshot) {
   const data = snapshot.val();
   
+  // Memperbarui nilai sensor di halaman
   document.getElementById('ph-value').innerText = data.ph || 'N/A';
   document.getElementById('suhu-value').innerText = data.suhu || 'N/A';
   document.getElementById('kekeruhan-value').innerText = data.kekeruhan || 'N/A';
   document.getElementById('tds-value').innerText = data.tds || 'N/A';
   
-  const date = new Date(data.timestamp * 1000); // Convert timestamp to readable date
+  const date = new Date(data.timestamp * 1000); // Mengubah timestamp ke format waktu yang bisa dibaca
   document.getElementById('timestamp-value').innerText = date.toLocaleString();
 }
 
-// Listen for changes in the Firebase Realtime Database
+// Mendengarkan perubahan data di Firebase Realtime Database
 database.ref('/sensor').on('value', updateSensorData);
